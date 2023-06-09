868edbac4540657bb20d6741b89b428ef53d52f2
Jean Chidem <jeanchidem2@gmail.com>
2023-06-09 18:59:16 -0300

    [feat] prevent block view

---
+++ src/Game/Controllers/BlockCueView.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+using CrazySnooker.Game.Entities.Characters;
+
+namespace CrazySnooker.Game.Controllers
+{
+    public class BlockCueView : Area
+    {
+        private PoolCueController cueController;
+        [Export]
+        private Material material;
+
+        public override void _Ready()
+        {
+            cueController = GetParent<PoolCueController>();
+            if (cueController.isRemote) return;
+            Connect("body_entered", this, nameof(OnBodyEntered));
+            Connect("body_exited", this, nameof(OnBodyExited));
+        }
+
+        public void OnBodyEntered(Node node)
+        {
+            if (node.Name == "CreatureBody" && material != null) {
+                MeshInstance mesh = node.GetNode<MeshInstance>("../first_creature/Armature/Skeleton/body");
+                mesh.MaterialOverride = material;
+            }
+        }
+
+        public void OnBodyExited(Node node)
+        {
+            if (node.Name == "CreatureBody" && material != null) {
+                MeshInstance mesh = node.GetNode<MeshInstance>("../first_creature/Armature/Skeleton/body");
+                mesh.MaterialOverride = null;
+            }
+        }
+    }
+}
